0f1be8d33072f873762c3e78a4cbc680a914ea0e
Marcelo Criscuolo <criscuolo.marcelo@gmail.com>
2010-09-26 11:59:11 -0300

    média final SEM SUB

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,21 @@ import br.usp.icmc.desempenho.DesempenhoPrinter;
 public class Main {
 	public static void main(String[] args) {
 		final DesempenhoPrinter printer = new DesempenhoPrinter();
+		Desempenho desempenho = readDesempenho();
+		
+		printer.print(desempenho);
+	}
+
+	private static Desempenho readDesempenho() {
 		Desempenho desempenho = new Desempenho();
 		desempenho.setNusp(4511488);
 		desempenho.setNotaProva1(3.7);
 		desempenho.setNotaProva2(5.2);
 
+		desempenho.addNotaTrabalho(8);
+		desempenho.addNotaTrabalho(7.5);
+		desempenho.addNotaTrabalho(9);
 		
-		
-		printer.print(desempenho);
+		return desempenho;
 	}
 }
--- src/br/usp/icmc/desempenho/Desempenho.java
+++ src/br/usp/icmc/desempenho/Desempenho.java
@@ -17,14 +17,23 @@ public class Desempenho {
 		return (2 * calcularMediaTrabalhos() + 3 * calcularMediaProvas()) / 5; 
 	}
 
-	private int calcularMediaProvas() {
-		// TODO Auto-generated method stub
-		return 0;
+	private double calcularMediaProvas() {
+		return (notaProva1 + notaProva2) / 2;
 	}
 
-	private int calcularMediaTrabalhos() {
-		// TODO Auto-generated method stub
-		return 0;
+	private double calcularMediaTrabalhos() {
+		double media = 0;
+		
+		if (notasTrabalhos.size() > 0) {
+			double soma = 0;
+			for (double nota : notasTrabalhos) {
+				soma += nota;
+			}
+			
+			media = soma / notasTrabalhos.size();
+		}
+		
+		return media;
 	}
 
 	public int getNusp() {
@@ -51,4 +60,12 @@ public class Desempenho {
 		this.notaProva2 = notaProva2;
 	}
 	
+	public void addNotaTrabalho(double nota) {
+		/* Encapsulamento!
+		 * O notasTrabalhos pode até ser trocado por um array. Expondo apenas 
+		 * o método addNotaTrabalho() você pode fazer essa troca sem quebrar o código
+		 * que usa essa classe. 
+		 */
+		notasTrabalhos.add(nota);
+	}
 }
